b92376c09ae17e87543dfbceb9933855ecbb2de8
Sheema Syed <shma.syd975@gmail.com>
2025-10-06 21:00:51 +0530

    Modified

---
+++ lib/test_logout.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'account/logout_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Logout Test',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: LogoutScreen(),
+    );
+  }
+}
